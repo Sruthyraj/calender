5ae34ad058bf996e52da484996876055d4747736
Sruthy Raj <sruthy>
2021-05-26 21:09:22 +0530

    final

--- script.js
+++ script.js
@@ -73,19 +73,11 @@ function Cal(month, year) {
       } else {
         let cell = document.createElement("td");
         let d = new Date(year, month, date);
-        // console.log(d);
 
-        // let data=document.getElementsByTagName("td").innerHTML=d
-        // cell.setAttribute("value", d);
-
-        // document.getElementsByTagName("td").innerHTML=d
         cell.setAttribute("class", "cellitem");
 
         let td_Cell = document.querySelector(".cellitem");
         cell.setAttribute("onclick", 'nav( " ' + d + ' " )');
-
-        // \"(canLaunch(\'' + v.LibraryItemName  + '\'))"\
-        // console.log(data);
 
         let cellText = document.createTextNode(date);
 
@@ -100,37 +92,97 @@ function Cal(month, year) {
     tbl.appendChild(row);
   }
   nav = (data) => {
-    // let d=document.getElementsByTagName("td").value
-
-    console.log(data);
-
     //popup
     model_Bg.classList.add("bg-active");
-    let listArray = document.querySelector(".list");
-
-    let formField = document.querySelector(".form");
-    let inputField = document.querySelector(".listItem");
-
-    formField.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      listArray.innerHTML += "<li>" + inputField.value + "</li>";
-
-      localStorage.setItem(data, listArray.innerHTML);
-    });
-
-    let allItems = localStorage.getItem(data);
-
-    console.log(allItems);
-    if (allItems) {
-      listArray.innerHTML = allItems;
-    }
-
+    model_Bg.classList.add("bg-active");
     cls_Btn.addEventListener("click", function () {
       model_Bg.classList.remove("bg-active");
       window.location.reload();
     });
+    let listArray = document.querySelector(".list");
 
-    // inputField.value="";
+    let formField = document.querySelector(".form");
+    let inputField = document.querySelector(".listItem");
+    let btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", function () {
+      let inputField = document.querySelector(".listItem");
+      let items = localStorage.getItem(data);
+      items = items ? items.split(",") : [];
+      items.push(inputField.value);
+      // console.log(items);
+      localStorage.setItem(data, items);
+      alert("event added sucessfully");
+      model_Bg.classList.remove("bg-active");
+      window.location.reload();
+    });
+
+    let allItems = localStorage.getItem(data);
+    // console.log(typeof allItems);
+    let newEle = [];
+
+    // console.log(allItems);
+    if (allItems) {
+      // listArray.innerHTML = allItems;1
+      newEle = allItems.split(",");
+      // console.log(newEle);
+
+      for (i = 0; i < newEle.length; i++) {
+        let item = document.createElement("li");
+        text = newEle[i];
+        console.log(text);
+        item.appendChild(document.createTextNode(newEle[i]));
+
+        listArray.appendChild(item);
+        let editBtn = document.createElement("button");
+        let deleteBtn = document.createElement("button");
+
+        editBtn.innerHTML = "edit";
+        editBtn.addEventListener(
+          "click",
+          (deleteFunction = () => {
+            let value = prompt("event ?");
+
+            let item = document.createElement("li");
+
+            item.appendChild(document.createTextNode(value));
+            let items = localStorage.getItem(data);
+            items = items ? items.split(",") : [];
+            items.push(value);
+            // console.log(items);
+            localStorage.setItem(data, items);
+
+            listArray.appendChild(item);
+
+            model_Bg.classList.remove("bg-active");
+            alert("Event Added Sucessfully");
+            window.location.reload();
+          })
+        );
+        item.appendChild(editBtn);
+        deleteBtn.innerHTML = "X";
+        item.addEventListener(
+          "click",
+          (deleteFunction = () => {
+            listArray.removeChild(item);
+            value = item.innerText;
+            console.log(value);
+
+            value = value.slice(0, -5);
+            console.log(value);
+            let items = localStorage.getItem(data);
+            console.log(items);
+            items = items ? items.split(",") : [];
+            console.log(items);
+            if (items.includes(value)) {
+              items.splice(items.indexOf(value), 1);
+            }
+
+            localStorage.setItem(data, items);
+          })
+        );
+        item.appendChild(deleteBtn);
+      }
+    }
   };
 }
